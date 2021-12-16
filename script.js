caed01a4df886a35f72b893e96cdcace89d3460f
dearshokhrukh <example@gmail.com>
2021-12-17 01:38:19 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+// const number = 9;
+// console.log(-4/0);
+// const firstName = "samar";
+
+
+// const xxx = 6+5;
+
+// console.log(xxx);
+
+// const person = {
+//   name: "shoh",
+//   age: 28,
+//   lanuage: 'uz',
+// };
+// console.log(person.lanuage);
+
+// const arr = ["red", "yellow", "blue", "black" ];
+// console.log(arr[3]);
+// const useage =  confirm("siz 18 yoshmisiz");
+// console.log(useage);
+// const namee = prompt("ismiyiz nima", "shoh ");
+// console.log(namee);
+
+// const answers = [];
+// answers[0] = prompt('yoshiz nechi');
+// answers[1] = prompt('ismiz nima');
+// answers[2] = prompt('qayirdansiz');
+// answers[3] = prompt('');
+// console.log(answers);
+
+const person = 'samar';
+console.log("mani ismim " + person);
